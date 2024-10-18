e0d3f2414c1a24a84f5669397e292a77efb2e030
Yifan Yang <yifany5@nycstudents.net>
2024-10-18 18:49:37 -0400

    My changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph that has the id="error".
-
-
-
-
+let errorMsg = document.getElementsByTagName("button")[0];
+errorMsg.addEventListener("click", function() {
+    let element = document.getElementById("error");
+    element.innerHTML += "Something went wrong, please try again"
+});
